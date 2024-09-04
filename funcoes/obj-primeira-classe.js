22b1d11b7292476512e4473f7a024284354d7810
Lucas Ferreira <150602557+Lucass-ferreira@users.noreply.github.com>
2024-09-04 20:49:16 -0300

    obj primeira classe

--- funcoes/obj-primeira-classe.js
+++ funcoes/obj-primeira-classe.js
@@ -15,3 +15,24 @@ const obj = {
 }
 
 obj.callback()
+
+function fn2(n1) {
+
+    return function(n2){
+        return n1 * n2
+    }
+}
+
+const funcao2 = fn2(10)
+const mult = funcao2(2)
+
+console.log(mult)
+
+function fn3() {
+    return function _fn3() {
+        console.log("função retornada por parametro")
+    }
+}
+
+const funcao3 = fn3
+funcao3()
