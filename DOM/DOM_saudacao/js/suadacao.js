d6bd6233b2a522901f2fb0134c702dd9bba69f3b
Lucas Ferreira <lucasferreiragithub@gmail.com>
2024-09-11 12:26:02 -0300

    esconder elemento

--- DOM/DOM_saudacao/js/suadacao.js
+++ DOM/DOM_saudacao/js/suadacao.js
@@ -1,4 +1,13 @@
 (function(){
-    const nomeUsuario = "Lucas"
-    document.querySelector(".top-bar p").textContent += nomeUsuario
+    const nomeUsuario = "lucas"
+    const elemento = document.querySelector(".top-bar p")
+    if(nomeUsuario) {
+        console.log(elemento.textContent)
+        elemento.innerHTML += "<b>" + nomeUsuario + "<b/>"
+    } else {
+        elemento.parentElement.style.display = "none"
+    }
+
+    console.log(elemento)
+    
 })()
