0c1b75e278a7365aac4070f055b6b13ea459b38b
Lucas Ferreira <lucasferreiragithub@gmail.com>
2024-09-11 12:53:47 -0300

    adicionando elemento

--- DOM/DOM_saudacao_append/js/suadacao.js
+++ DOM/DOM_saudacao_append/js/suadacao.js
@@ -1,5 +1,15 @@
 (function(){
     const nomeUsuario = null
     
+    if(nomeUsuario) {
+          const topBarElement = document.createElement("div")
+          topBarElement.className = "top-bar"
+
+          topBarElement.innerHTML = `<p> Olá, <b> ${nomeUsuario} </b> </p>`
+
+          //elemntoPai.insertBefore(novoElemento, elementoReferencial)
+          const elemntoPai = document.querySelector(".hero")
+          elemntoPai.insertBefore(topBarElement, elemntoPai.firstElementChild)
+    }
     
 })()
