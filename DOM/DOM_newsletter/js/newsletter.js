6d0ee2069f3c1252fa630eaa0770e8039acf7def
Lucas Ferreira <lucasferreiragithub@gmail.com>
2024-09-12 14:30:38 -0300

    newsletter/"validar e-mail"

--- DOM/DOM_newsletter/js/newsletter.js
+++ DOM/DOM_newsletter/js/newsletter.js
@@ -1,4 +1,14 @@
-(function () {
+// (function () {
+    
+// })()
 
+function clicar(){
+    let email = document.querySelector("#txtEmail")
+    let output = document.querySelector("#newsletterFeedback")
 
-})()
+    output.innerHTML = `email ${email.value} foi cadastrado com 
+    sucesso!`
+
+    let novoElemento = document.createElement("p")
+    
+}
