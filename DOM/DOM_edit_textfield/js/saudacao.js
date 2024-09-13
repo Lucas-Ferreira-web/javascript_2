f481001fe730f3d2a8526dedf0b04207b8883362
Lucas Ferreira <lucasferreiragithub@gmail.com>
2024-09-13 12:37:19 -0300

    editando o input

--- DOM/DOM_edit_textfield/js/saudacao.js
+++ DOM/DOM_edit_textfield/js/saudacao.js
@@ -1,4 +1,10 @@
-(function () {
+const textEmail = document.getElementById("txtEmail")
 
+function editarEmail(){
+    textEmail.disabled = false
+    textEmail.focus()
+}
 
-})()
+function perdeFoco(){
+    textEmail.disabled = true
+}
