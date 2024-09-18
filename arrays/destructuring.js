c11cb7dbc3c123e7e151da27dfccab25419688ee
Lucas Ferreira <lucasferreiragithub@gmail.com>
2024-09-18 13:36:59 -0300

    destructuring

---
+++ arrays/destructuring.js
@@ -0,0 +1,8 @@
+// serve tanto para array quanto para objeto
+const arr = [10, 20, 30]
+
+const [n1, n2] = arr // pega os valores na ordem
+// const [n1, ,n2] = arr // "," na frente do elemento pega o seu sucessor
+console.log(n1)
+console.log(n2)
+
