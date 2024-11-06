228d77e88f8cc0a3eb42f2f3c307950b64776b29
Lucas Ferreira <lucasferreiragithub@gmail.com>
2024-11-06 16:08:14 -0300

    slice e substring

--- Strings/introducao.js
+++ Strings/introducao.js
@@ -3,11 +3,20 @@ let str1 = "minha string bacanuda"
 console.log(str1.replace("i", "o")) // normal
 console.log(str1)
 
-console.log(str1.indexOf("string")) // retorna a primeira ocorrencia
-console.log(str1.lastIndexOf("i")) // retorna a ultima ocorrencia
-console.log(str1.indexOf("kjhsafjhs")) // se não existir nada ele retorna -1
-console.log(str1.includes("minha")) // usando includes
-console.log(str1.indexOf("minha") >= 0) // usando includes atravez do indexOf
+// console.log(str1.indexOf("string")) // retorna a primeira ocorrencia
+// console.log(str1.lastIndexOf("i")) // retorna a ultima ocorrencia
+// console.log(str1.indexOf("kjhsafjhs")) // se não existir nada ele retorna -1
+// console.log(str1.includes("minha")) // usando includes
+// console.log(str1.indexOf("minha") >= 0) // usando includes atravez do indexOf
+
+console.log(str1.slice(2, 7)) // 
+console.log(str1.substring(2, 7))
+
+console.log(str1.slice(-5, -2)) // aceita valores negativos
+console.log(str1.substring(-5, -2)) //não aceita valores negativos
+
+console.log(str1.slice(8, 1)) // não aceita numero incial maior q o numero final
+console.log(str1.substring(8, 1)) // aceita numero inicial menor q o numero final
 
 
 
