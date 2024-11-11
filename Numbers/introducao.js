fd9f042f75672aa60df86a246989e1cda281cac1
Lucas Ferreira <lucasferreiragithub@gmail.com>
2024-11-11 06:06:27 -0300

    toString() e toLocaleString()

--- Numbers/introducao.js
+++ Numbers/introducao.js
@@ -9,3 +9,17 @@ numero = 12.34567
 
 console.log(numero.toExponential()) // transforma o numero em notação cientifica, e recebe um paramentro para a quantidade de casas decimais no numero
 console.log(numero.toExponential(4))
+
+numero = 15
+
+console.log(numero.toString(2)) // base 2, numero binario
+console.log((2).toString(2))
+
+console.log((15).toString(16)) // base 16, numero exadecimal
+console.log((16).toString(16))
+
+numero = 123456.789
+console.log(numero.toLocaleString())
+console.log(numero.toLocaleString("pt-BR"))
+console.log(numero.toLocaleString("pt-BR", {style: "currency", currency: "BRL"}))
+console.log(numero.toLocaleString("pt-PT", {style: "currency", currency: "EUR"}))
