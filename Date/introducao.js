73cffe6d3642359bbb0c15489a2017490a1f6159
Lucas Ferreira <lucasferreiragithub@gmail.com>
2024-11-12 15:07:04 -0300

    date introdução

---
+++ Date/introducao.js
@@ -0,0 +1,10 @@
+const data = new Date()
+
+console.log(data)
+console.log(data.getDay())
+console.log(data.getDate())
+console.log(data.getMonth())
+console.log(data.getTime())
+console.log(data.getFullYear())
+console.log(data.getHours())
+console.log(data.getUTCHours()) // UTC hora universal
