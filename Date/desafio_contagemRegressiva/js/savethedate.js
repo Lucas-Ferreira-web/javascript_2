19b86378727adda6f256d763c2d36808e7c8c384
Lucas Ferreira <lucasferreiragithub@gmail.com>
2024-11-13 21:38:54 -0300

    desafio contagem regressiva

---
+++ Date/desafio_contagemRegressiva/js/savethedate.js
@@ -0,0 +1,9 @@
+;(function () {
+    const datAtual = document.querySelector("#dataRegre")
+    console.log(datAtual.textContent.trim().split(" ")[0])
+
+})()
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const datAtual = document.querySelector("#dataRegre")
+// })
